407b57eabd184154a4016499421c4d9f8676ab59
daengdaengLee <gunho1020@gmail.com>
2019-02-08 17:06:18 +0900

    ENH: Modifiy entry index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+import "./index.css";
+import "./components/pages/main-page/main-page.js";
+
+document.querySelector("body").innerHTML =
+  "<my-app-main-page></my-app-main-page>";
